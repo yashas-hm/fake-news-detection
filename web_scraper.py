b59ed8c694833c576c743c6b4d6dd618715887cd
Yashas Majmudar <yashashm@gmail.com>
2022-08-26 18:38:09 +0530

    final

--- web_scraper.py
+++ web_scraper.py
@@ -1,3 +1,5 @@
+import re
+
 import requests
 from bs4 import BeautifulSoup
 import json
@@ -29,3 +31,47 @@ class WebScraper:
                     return df
             except:
                 pass
+
+    @staticmethod
+    def gov_site(url):
+        df = pd.DataFrame(columns=['title', 'text', 'subject'])
+        headers = {
+            'User-Agent':
+                'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                'Chrome/51.0.2704.103 Safari/537.36', }
+        data = requests.get(url, headers=headers)
+        soup = BeautifulSoup(data.content, 'html.parser')
+        scripts = soup.find_all('input')
+        title = ''
+        subject = 'Policy'
+        text = ''
+        for index in range(len(scripts)):
+            element = str(scripts[index])
+            print(element)
+            if element.find('name="ltrTitlee"') != -1 and element.find('id="ltrTitlee"') != -1:
+                element = element.replace('<input', '')
+                element = element.replace('type="hidden"', '')
+                element = element.replace('name="ltrTitlee"', '')
+                element = element.replace('value="', '')
+                element = element.replace('id="ltrTitlee"', '')
+                element = element.replace('"/>', '')
+                title = element
+            elif element.find('name="ltrDescriptionn"') != -1 and element.find('id="ltrDescriptionn"'):
+                element = element.replace('<input', '')
+                element = element.replace('type="hidden"', '')
+                element = element.replace('name="ltrDescriptionn"', '')
+                element = element.replace('id="ltrDescriptionn"', '')
+                element = element.replace('value="', '')
+                element = element.replace('\'/>', '')
+                element = re.sub(r'^&lt;[a-zA-Z\/;&"=_.: ].*&gt;$', '', element)
+                text = element
+
+        df.loc[0] = [title, text, subject]
+        print(df.text[0])
+        print(df.title[0])
+        return df
+
+
+# a = WebScraper.economic_times_scraper('https://economictimes.indiatimes.com/news/india/sc-sets-aside-mp-hc-verdict-on-discharge-of-rape-accused-says-its-utterly-incomprehensible/articleshow/93659916.cms')
+# a = WebScraper.gov_site('https://pib.gov.in/PressReleaseIframePage.aspx?PRID=1854323')
+# print(a)
